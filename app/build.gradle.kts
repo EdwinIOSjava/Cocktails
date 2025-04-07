b201fca79e793ab7c9ac616474bff9b44d534d54
IOSEdwin <eramirezn23@icloud.com>
2025-04-07 12:52:04 +0200

    Second commit icon builded

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.proytectmine"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.proytectmine"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
