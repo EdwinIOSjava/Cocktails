16dd1acb9e4ad6bed6db569f18941feb92a21138
IOSEdwin <eramirezn23@icloud.com>
2025-04-21 13:21:54 +0200

    creado el spinner o menu desplegable

--- app/src/main/java/com/example/proytectmine/activities/IngredientActivity.kt
+++ app/src/main/java/com/example/proytectmine/activities/IngredientActivity.kt
@@ -1,23 +1,141 @@
 package com.example.proytectmine.activities
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.proytectmine.R
+import com.example.proytectmine.databinding.ActivityDetailBinding
+import com.example.proytectmine.databinding.ActivityIngredientBinding
 import com.google.android.material.textfield.MaterialAutoCompleteTextView
 
 class IngredientActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityIngredientBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_ingredient)
+        binding = ActivityIngredientBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val items = listOf("Item 1", "Item 2", "Item 3", "Item 4")
+        val ingredientes = arrayOf(
+            "Light rum",
+            "Bourbon",
+            "Vodka",
+            "Gin",
+            "Blended whiskey",
+            "Tequila",
+            "Sweet Vermouth",
+            "Apricot brandy",
+            "Triple sec",
+            "Southern Comfort",
+            "Orange bitters",
+            "Brandy",
+            "Lemon vodka",
+            "Dry Vermouth",
+            "Dark rum",
+            "Amaretto",
+            "Tea",
+            "Applejack",
+            "Champagne",
+            "Scotch",
+            "Coffee liqueur",
+            "Añejo rum",
+            "Bitters",
+            "Sugar",
+            "Kahlua",
+            "Dubonnet Rouge",
+            "Lime juice",
+            "Irish whiskey",
+            "Apple brandy",
+            "Carbonated water",
+            "Cherry brandy",
+            "Creme de Cacao",
+            "Grenadine",
+            "Port",
+            "Coffee brandy",
+            "Red wine",
+            "Rum",
+            "Grapefruit juice",
+            "Ricard",
+            "Sherry",
+            "Cognac",
+            "Sloe gin",
+            "Strawberry schnapps",
+            "Apple juice",
+            "Pineapple juice",
+            "Lemon juice",
+            "Sugar syrup",
+            "Milk",
+            "Strawberries",
+            "Chocolate syrup",
+            "Yoghurt",
+            "Mango",
+            "Ginger",
+            "Lime",
+            "Cantaloupe",
+            "Berries",
+            "Grapes",
+            "Kiwi",
+            "Tomato juice",
+            "Cocoa powder",
+            "Chocolate",
+            "Heavy cream",
+            "Galliano",
+            "Peach Vodka",
+            "Ouzo",
+            "Coffee",
+            "Spiced rum",
+            "Water",
+            "Espresso",
+            "Angelica root",
+            "Orange",
+            "Cranberries",
+            "Johnnie Walker",
+            "Apple cider",
+            "Everclear",
+            "Cranberry juice",
+            "Egg yolk",
+            "Egg",
+            "Grape juice",
+            "Peach nectar",
+            "Lemon",
+            "Firewater",
+            "Lemonade",
+            "Lager",
+            "Whiskey",
+            "Absolut Citron",
+            "Pisco",
+            "Irish cream",
+            "Ale",
+            "Chocolate liqueur",
+            "Midori melon liqueur",
+            "Sambuca",
+            "Cider",
+            "Sprite",
+            "7-Up",
+            "Blackberry brandy",
+            "Peppermint schnapps",
+            "Creme de Cassis",
+            "Jack Daniels",
+            "Baileys irish cream"
+        )
+        val adapter = ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, ingredientes)
+
+        (binding.menu.editText as? AutoCompleteTextView)?.setAdapter(adapter)
+
+
+
 
     }
 }
